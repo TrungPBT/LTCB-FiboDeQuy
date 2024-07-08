2a41fc95b6bfdaeff54615fff33b068b36ecbea9
SPC <SPC@25-LAB402>
2024-07-08 18:52:17 +0700

    Add project files.

---
+++ LTCB-FiboDeQuy.cpp
@@ -0,0 +1,39 @@
+// LTCB-FiboDeQuy.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+#include <iostream>
+
+using namespace std;
+
+int fibonacci(int n) {
+    if (n < 0) {
+        return -1;
+    }
+    else if (n == 0 || n == 1) {
+        return n;
+    }
+    else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+
+int main() {
+    int i;
+    cout << "10 so dau tien cua day so Fibonacci: \n";
+    for (i = 0; i < 10; i++) {
+        cout << fibonacci(i) << " ";
+    }
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
